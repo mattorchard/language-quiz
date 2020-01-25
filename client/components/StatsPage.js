de77ad50536b9aef0e1ccc1a066e868fbb405383
Matt Orchard <morch028@uottawa.ca>
2020-01-25 11:29:19 -0500

    Show raw question data on stats page

--- client/components/StatsPage.js
+++ client/components/StatsPage.js
@@ -1,11 +1,17 @@
-import greek from "../datasheets/greek";
+import { getQuestions } from "../repositories/questionRepository";
+import { useMemo } from "preact/hooks";
 
-const StatsPage = () => (
-  <main>
-    <h2>Stats</h2>
-    <p>No Stats are being tracked yet, so here's the raw data being used</p>
-    <pre>{JSON.stringify(greek, null, 2)}</pre>
-  </main>
-);
+const useStats = () => useMemo(() => getQuestions(), []);
+
+const StatsPage = () => {
+  const questions = useStats();
+  return (
+    <main>
+      <h2>Stats</h2>
+      <p>No Stats are being tracked yet, so here's the raw data being used</p>
+      <pre>{JSON.stringify(questions, null, 2)}</pre>
+    </main>
+  );
+};
 
 export default StatsPage;
