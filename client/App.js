ab99e8272215aa7fcf678fe90d8dfadc1b596960
Matt Orchard <morch028@uottawa.ca>
2020-01-24 21:08:27 -0500

    Adds prettier, and applies formatting

--- client/App.js
+++ client/App.js
@@ -1,27 +1,32 @@
-import {render, Fragment} from "preact";
+import { render, Fragment } from "preact";
 import useHash from "./hooks/useHash";
 import HomePage from "./components/HomePage";
 import QuizPage from "./components/QuizPage";
 import StatsPage from "./components/StatsPage";
 
-
 const App = () => {
   const hash = useHash();
 
-  return <Fragment>
-    <header>
-      <h1>Word Variations</h1>
-      <nav>
-        <a href="#home">Home</a>
-        <a href="#quiz">Quiz</a>
-        <a href="#stats">Stats</a>
-      </nav>
-    </header>
-    {(!hash || hash === "#home") && <HomePage/>}
-    {hash === "#quiz" && <QuizPage/>}
-    {hash === "#stats" && <StatsPage/>}
-  </Fragment>;
+  return (
+    <Fragment>
+      <header>
+        <h1>Word Variations</h1>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#quiz">Quiz</a>
+          <a href="#stats">Stats</a>
+        </nav>
+      </header>
+      {(!hash || hash === "#home") && <HomePage />}
+      {hash === "#quiz" && <QuizPage />}
+      {hash === "#stats" && <StatsPage />}
+    </Fragment>
+  );
 };
 
-const AppWrapper = <Fragment><App/></Fragment>
+const AppWrapper = (
+  <Fragment>
+    <App />
+  </Fragment>
+);
 render(AppWrapper, document.querySelector("#appRoot"));
