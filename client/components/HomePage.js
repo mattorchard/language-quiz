4c21a3d598230b04517b8dc0aa8b86f3dfe2e27f
Matt Orchard <morch028@uottawa.ca>
2020-01-24 21:07:51 -0500

    Adds placeholder content

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,5 +1,6 @@
 const HomePage = () => <main>
   <h2>Home</h2>
+  <p>This is the home page, it might say something at some point. For now it just says this</p>
 </main>;
 
 export default HomePage;
--- client/components/StatsPage.js
+++ client/components/StatsPage.js
@@ -1,5 +1,11 @@
-const StatsPage = () => <main>
-  <h2>Stats</h2>
-</main>;
+import greek from "../datasheets/greek";
+
+const StatsPage = () => (
+  <main>
+    <h2>Stats</h2>
+    <p>No Stats are being tracked yet, so here's the raw data being used</p>
+    <pre>{JSON.stringify(greek, null, 2)}</pre>
+  </main>
+);
 
 export default StatsPage;
